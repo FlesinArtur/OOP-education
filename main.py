43afb7215b7902fe6f61fc71592b38545f4a62f1
Artur Flesin <flesinartur.com>
2023-01-22 20:52:30 +0200

    added class Number which print 'Number(number)'

---
+++ main.py
@@ -0,0 +1,13 @@
+class Number:
+
+    def __init__(self, number):
+        self.number = number
+        print(f"Number ({number})")
+
+
+def main():
+    mynumber = Number(9)
+
+
+if __name__ == "__main__":
+    main()
