1d387ec0abc883b325ed83a30f84a4ebc660daa5
Artur Flesin <flesinartur.com>
2023-01-22 22:00:04 +0200

    reloading the add function

--- main.py
+++ main.py
@@ -4,9 +4,14 @@ class Number:
         self.number = number
         print(f"Number ({number})")
 
+    def __add__(self, value):
+        print(self.number + value.number)
+
 
 def main():
     mynumber = Number(9)
+    mynumber1 = Number(6)
+    mynumber + mynumber1
 
 
 if __name__ == "__main__":
