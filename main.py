c5abe87065b0cc3f417283ee22eca7ec9ccf1d3c
Artur Flesin <flesinartur.com>
2023-01-23 15:29:03 +0200

    fix validation

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ class Number:
 
     def __init__(self, number):
         self.number = number
-        if type(number) is str:
+        if type(number) is not int:
             try:
                 self.number = int(number)
-            except ValueError:
-                print('Error')
-                sys.exit()
+            except (ValueError, TypeError):
+                raise Exception("Неправильно введені дані!")
+
         print(f"Number ({number})")
 
     def __add__(self, value):
@@ -18,7 +18,7 @@ class Number:
 
 
 def main():
-    mynumber = Number('5')
+    mynumber = Number(6)
     mynumber1 = Number(6)
     mynumber + mynumber1
 
