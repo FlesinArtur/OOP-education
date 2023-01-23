905cd4e0fa1b4cbc07e05848fa0d2ad1eb585bad
Artur Flesin <flesinartur.com>
2023-01-23 21:07:27 +0200

    add many dunder methods

--- main.py
+++ main.py
@@ -10,17 +10,33 @@ class Number:
                 self.number = int(number)
             except (ValueError, TypeError):
                 raise Exception("Неправильно введені дані!")
-
         print(f"Number ({number})")
 
+    def __str__(self):
+        return f"Number({self.number})"
+
     def __add__(self, value):
-        print(self.number + value.number)
+        return Number(self.number + value.number)
+
+    def __sub__(self, value):
+        return Number(self.number - value.number)
+
+    def __mul__(self, value):
+        return Number(self.number * value.number)
+
+    def __truediv__(self, value):
+        return Number(self.number / value.number)
 
 
 def main():
-    mynumber = Number(6)
+    mynumber = Number('12')
     mynumber1 = Number(6)
-    mynumber + mynumber1
+    print(mynumber)
+    print(mynumber1)
+    my_value = mynumber + mynumber1
+    my_value = mynumber * mynumber1
+    my_value = mynumber - mynumber1
+    my_value = mynumber / mynumber1
 
 
 if __name__ == "__main__":
