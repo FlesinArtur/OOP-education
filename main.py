d9237e03005e65aefacf4423e61fe55fddf8cd1c
Artur Flesin <artifloid@gmail.com>
2023-02-05 18:07:41 +0200

    add decorators

--- main.py
+++ main.py
@@ -1,4 +1,26 @@
-import sys
+import time
+
+
+def stopwatch(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time() - start
+        print(*args, **kwargs)
+        print(f"Time of function end: {end}")
+        return result
+    return wrapper
+
+
+def list_count(count):
+    def list_count_(func):
+        def wrapper(*args, **kwargs):
+            result = []
+            for i in range(count):
+                result.append(func(*args, **kwargs))
+            return print(result)
+        return wrapper
+    return list_count_
 
 
 class A:
@@ -24,12 +46,15 @@ class Number(A, B):
     def __str__(self):
         return f"Number({self.number})"
 
+    @stopwatch
     def __add__(self, value):
         return Number(self.number + value.number)
 
+    @stopwatch
     def __sub__(self, value):
         return Number(self.number - value.number)
 
+    @list_count(3)
     def __mul__(self, value):
         return Number(self.number * value.number)
 
@@ -43,15 +68,14 @@ class Number(A, B):
 def main():
     mynumber = Number('12')
     mynumber1 = Number(6)
-    print(mynumber)
-    print(mynumber1)
+   #print(mynumber)
+   #print(mynumber1)
     my_value = mynumber + mynumber1
     my_value = mynumber * mynumber1
-    my_value = mynumber - mynumber1
-    my_value = mynumber / mynumber1
-
-    print(f'{mynumber1.x}=')    # f-strings
-    print(f'{mynumber1.x=}')    # print(f'mynumber1.x={mynumber1.x}')
+   #my_value = mynumber - mynumber1
+   #my_value = mynumber / mynumber1
+   #print(f'{mynumber1.x}=')    # f-strings
+   #print(f'{mynumber1.x=}')    # print(f'mynumber1.x={mynumber1.x}')
 
 
 def test():
@@ -59,12 +83,5 @@ def test():
 
 
 if __name__ == "__main__":
-    a = test
 
-    # print(test == a)
-
-    # The same
-    print(id(test) == id(a))
-    print(test is a)
-
-    # main()
+    main()
